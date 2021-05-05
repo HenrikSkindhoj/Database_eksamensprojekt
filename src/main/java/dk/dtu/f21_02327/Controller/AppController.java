654354bd52d8644c80fae01ceb98147c3592a103
Kageschwump <kasperfalch@live.dk>
2021-05-06 01:50:58 +0200

    lavet en en lille brugerinteraktion

--- src/main/java/dk/dtu/f21_02327/Controller/AppController.java
+++ src/main/java/dk/dtu/f21_02327/Controller/AppController.java
@@ -7,8 +7,10 @@ import dk.dtu.f21_02327.Model.VaccineRapport;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AppController {
 
@@ -18,35 +20,62 @@ public class AppController {
         VaccinationsMapper vaccinationsMapper = new VaccinationsMapper(connection);
         PersonMapper personMapper = new PersonMapper(connection);
         List<VaccinationsAftale> aftaler = null;
+        System.out.println("Hvad vil du foretage dig?");
+        System.out.println("For at indlæse vaccinations aftaler, tast \"LD\"");
+        System.out.println("For at printe en daglig rapport, tast \"PD\"");
+        System.out.println("For at printe en månedlig rapport, tast \"PM\"");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
 
-        try {
+        switch (command){
+            case "LD":
+                try {
 
-            aftaler = indlaes.indlaesAftaler("src/main/resources/docs/vaccinationsaftaler.csv");
-            
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+                    aftaler = indlaes.indlaesAftaler("src/main/resources/docs/vaccinationsaftaler.csv");
+
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+                for (VaccinationsAftale aftale: aftaler) {
+
+                    Person newPerson = new Person(aftale.getCprnr(),aftale.getNavn());
+                    personMapper.createPersonInDB(newPerson);
+
+                }
+
+                for (VaccinationsAftale aftale : aftaler) {
+                    vaccinationsMapper.createAftaleInDB(aftale);
+                }
+                try{
+                    connection.getConnection().close();
+                }catch (SQLException e)
+                {
+                    e.printStackTrace();
+                    System.err.println("Error closing the connection to Database");
+                }
+                break;
+
+            case "PD":
+                try {
+                    createRepports();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+                break;
+
+            case "PM":
+                System.out.println("TODO");
+
+            default:
+                System.out.println("nice meme.");
         }
 
-        for (VaccinationsAftale aftale: aftaler) {
-
-            Person newPerson = new Person(aftale.getCprnr(),aftale.getNavn());
-            personMapper.createPersonInDB(newPerson);
-
         }
 
-        for (VaccinationsAftale aftale : aftaler) {
-            vaccinationsMapper.createAftaleInDB(aftale);
-        }
-        try{
-            connection.getConnection().close();
-        }catch (SQLException e)
-        {
-            e.printStackTrace();
-            System.err.println("Error closing the connection to Database");
-        }
 
-    }
 
     public void createRepports() throws SQLException {
         DBConnection connection = new DBConnection();
